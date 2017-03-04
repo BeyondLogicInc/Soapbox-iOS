743786b359317a1bbffd9911162209a5e642ccb9
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-03-04 11:51:37 +0530

    Added keyboard hide login on tap outside textField.

--- Soapbox/LoginViewController.swift
+++ Soapbox/LoginViewController.swift
@@ -8,26 +8,50 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
         
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         username.setBottomBorder()
         password.setBottomBorder()
+        
+        self.hideKeyboard()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.username {
+            if self.username.returnKeyType == UIReturnKeyType.next {
+                self.password.becomeFirstResponder()
+            }
+        }
+        else if textField == self.password {
+            if self.password.returnKeyType == UIReturnKeyType.go {
+                login()
+            }
+        }
+        return true
+    }
+    
+    
     @IBAction func handleLogin(_ sender: Any) {
+        login()
+    }
+    
+    func login() {
         self.performSegue(withIdentifier: "toTabViewSegue", sender: self)
     }
+    
 }
 
 extension UITextField {
@@ -38,5 +62,18 @@ extension UITextField {
         bottomLine.frame = CGRect(x: 0.0, y: self.frame.height - 1, width: self.frame.width, height: 1.0)
         bottomLine.backgroundColor = UIColor.lightGray.cgColor
         self.layer.addSublayer(bottomLine)
+    }
+}
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard)
+        )
+        view.addGestureRecognizer(tap)
+    }
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
