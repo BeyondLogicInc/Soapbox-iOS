730d13937e5b96c41751e8b160f00ea8dea107ed
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-03-04 11:02:50 +0530

    Added a new LoginViewController for Login view.

---
+++ Soapbox/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Soapbox
+//
+//  Created by Atharva Dandekar on 3/4/17.
+//  Copyright © 2017 BeyondLogic. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        username.setBottomBorder()
+        password.setBottomBorder()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func handleLogin(_ sender: Any) {
+        self.performSegue(withIdentifier: "toTabViewSegue", sender: self)
+    }
+}
+
+extension UITextField {
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.clear.cgColor
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: self.frame.height - 1, width: self.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.addSublayer(bottomLine)
+    }
+}
