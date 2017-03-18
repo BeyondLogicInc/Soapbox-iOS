bc6e26b07de33e946ed5efc6115ebe48ff080a4d
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-03-18 22:20:32 +0530

    deselect row in didSelectRow ReadingListVC.

--- Soapbox/ReadingListViewController.swift
+++ Soapbox/ReadingListViewController.swift
@@ -100,4 +100,8 @@ class ReadingListViewController: UIViewController, UITableViewDataSource, UITabl
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
