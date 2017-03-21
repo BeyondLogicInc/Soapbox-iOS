6cdc5a14fee3c5d6e1097779533091b3d6f8261a
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-03-21 12:49:39 +0530

    Created new file for Featured ThreadsVC.

---
+++ Soapbox/FeaturedThreadsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeaturedThreadsViewController.swift
+//  Soapbox
+//
+//  Created by Atharva Dandekar on 3/21/17.
+//  Copyright © 2017 BeyondLogic. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedThreadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
