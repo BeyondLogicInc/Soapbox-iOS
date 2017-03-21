c46a009dcd320a0ad0fdeda8fa4c68b5f8447c71
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-03-21 13:07:37 +0530

    Added custom FeaturedThreadsCell.

---
+++ Soapbox/FeaturedThreadsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeaturedThreadsTableViewCell.swift
+//  Soapbox
+//
+//  Created by Atharva Dandekar on 3/21/17.
+//  Copyright © 2017 BeyondLogic. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedThreadsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
